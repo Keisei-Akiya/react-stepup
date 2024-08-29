d4e7614ac2695e4861bd28befe797aeabedce365
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-29 19:49:18 +0900

    Query Parameter

--- react-router/src/UrlParameter.jsx
+++ react-router/src/UrlParameter.jsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+
   return (
     <div>
       <h1>UrlParameter!</h1>
       <p>parameter = {id}</p>
+      <p>query parameter = {query.get("name")}</p>
     </div>
   );
 };
