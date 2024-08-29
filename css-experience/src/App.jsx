86bbf8726c04b5dc6912a548fd06246eaf215fc8
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-29 17:14:29 +0900

    様々なCSSのあてかたに触れた． - Inline Styles - CSS Modules - Styled Jsx - Styled Components - Emotion

---
+++ css-experience/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { CssModules } from "./components/CssModules";
+import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
+
+function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
+    </div>
+  );
+}
+
+export default App;
