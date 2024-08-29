3c11e43c3d01ddcde61c8f2c4c51e6d6163d26e4
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-29 19:43:37 +0900

    URL Parameter id別に割り当てるやつの始めをやった．

--- react-router/src/router/Page1Routes.jsx
+++ react-router/src/router/Page1Routes.jsx
@@ -4,7 +4,7 @@ import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetailB";
 
 export const Page1Routes = [
-  <Route path="/page1" element={<Page1 />}>
+  <Route key="page1" path="/page1" element={<Page1 />}>
     <Route path="detailA" element={<Page1DetailA />} />
     <Route path="detailB" element={<Page1DetailB />} />
   </Route>,
--- react-router/src/router/Page2Routes.jsx
+++ react-router/src/router/Page2Routes.jsx
@@ -1,4 +1,8 @@
 import { Route } from "react-router-dom";
 import { Page2 } from "../Page2";
+import { UrlParameter } from "../UrlParameter";
 
-export const Page2Routes = [<Route path="/page2" element={<Page2 />} />];
+export const Page2Routes = [
+  <Route key="page2" path="/page2" element={<Page2 />} />,
+  <Route key="page2-with-id" path="/page2/:id" element={<UrlParameter />} />,
+];
