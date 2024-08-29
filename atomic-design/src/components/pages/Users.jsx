bcd9a191b2e3ba76cbefa0cc0b4ee7cc59087611
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-29 23:55:14 +0900

    Atomic Design Complete

---
+++ atomic-design/src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `Homura${val}`,
+    image:
+      "https://plus.unsplash.com/premium_photo-1682095958942-77b7a7764176?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    email: "12345@example.com",
+    phone: "000-1111-2222",
+    company: {
+      name: "Test.inc",
+    },
+    website: "https://google.com",
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>USERS</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
