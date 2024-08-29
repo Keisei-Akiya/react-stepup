23bb06cf7058b8b06b7d444610e3331501df46ac
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-29 20:01:22 +0900

    stateを渡す

--- react-router/src/Page1.jsx
+++ react-router/src/Page1.jsx
@@ -3,13 +3,19 @@ import { Link, Outlet, useLocation } from "react-router-dom";
 export const Page1 = () => {
   const location = useLocation();
   const isDetail = location.pathname.includes("detail");
+
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+
   return (
     <div>
       {!isDetail && (
         <>
           <h1>Page1</h1>
           <nav>
-            <Link to="detailA">Detail A</Link>
+            <Link to="detailA" state={{ arr }}>
+              Detail A
+            </Link>
             <br />
             <Link to="detailB">Detail B</Link>
           </nav>
--- react-router/src/Page1DetailA.jsx
+++ react-router/src/Page1DetailA.jsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const location = useLocation();
+  const { arr } = location.state || {};
+  console.log(arr);
+
   return (
     <div>
       <h1>Page1DetailA</h1>
