95d040e5b292c2e4d5705d7a2f451e14cf41cb80
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-30 11:09:50 +0900

    globalなState管理

--- atomic-design/src/components/pages/Top.jsx
+++ atomic-design/src/components/pages/Top.jsx
@@ -1,9 +1,28 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { SecondaryButton } from "../atoms/buttons/SecondaryButton";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
+  const navigate = useNavigate();
+  const { setUserInfo } = useContext(UserContext);
+
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    navigate("/users");
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    navigate("/users");
+  };
   return (
     <SContainer>
       <h2>TOP</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
