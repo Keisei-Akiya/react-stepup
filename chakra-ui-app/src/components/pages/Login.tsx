93eeb2310c2318067562aa893a35e3fd61da2506
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-31 14:51:50 +0900

    PrimaryButtonを自力でコンポーネント化

--- chakra-ui-app/src/components/pages/Login.tsx
+++ chakra-ui-app/src/components/pages/Login.tsx
@@ -1,5 +1,20 @@
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { memo } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login = memo(() => {
-  return <p>Login Page</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          User Management App
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="User ID" />
+          <PrimaryButton>Login</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
