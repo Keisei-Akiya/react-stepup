9e245d3a6fcdec51b3077af5694886fe1a9d7d2b
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-31 17:51:53 +0900

    ユーザー一覧作成

--- chakra-ui-app/src/components/pages/UserManagement.tsx
+++ chakra-ui-app/src/components/pages/UserManagement.tsx
@@ -1,5 +1,32 @@
-import { memo } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect } from "react";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement = memo(() => {
-  return <p>User Management Page</p>;
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), [getUsers]);
+
+  return (
+    <>
+      {loading ? (
+        <Center height="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://picsum.photos/800"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
